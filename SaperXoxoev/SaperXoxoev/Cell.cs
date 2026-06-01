57bcea611e16c593cab145f9a789e8355d6cca4a
xoxoevv <gogola15015gmail.com>
2026-06-01 19:42:14 +0400

    Добавил класс клеток

---
+++ SaperXoxoev/SaperXoxoev/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaperXoxoev
+{
+    public class Cell
+    {
+        public bool IsMine { get; set; }
+
+        public bool IsOpen { get; set; }
+
+        public bool IsFlag { get; set; }
+
+        public int NearMines { get; set; }
+    }
+}
