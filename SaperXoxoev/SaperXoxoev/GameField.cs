15b2a7ce04e1aeeac763b9db9894156ddb6615f9
xoxoevv <gogola15015gmail.com>
2026-06-01 19:43:33 +0400

    Добавил класс с массивом клеток

---
+++ SaperXoxoev/SaperXoxoev/GameField.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaperXoxoev
+{
+    public class GameField
+    {
+        public int Size { get; }
+
+        public Cell[,] Cells;
+
+        public GameField(int size)
+        {
+            Size = size;
+
+            Cells = new Cell[size, size];
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    Cells[y, x] = new Cell();
+                }
+            }
+        }
+
+        public void SetMine(int y, int x)
+        {
+            Cells[y, x].IsMine = true;
+        }
+
+        public Cell GetCell(int y, int x)
+        {
+            return Cells[y, x];
+        }
+    }
+}
