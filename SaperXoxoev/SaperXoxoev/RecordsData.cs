82193eb788504a523a70ca65c44dea04af0d8bea
xoxoevv <gogola15015gmail.com>
2026-06-24 12:18:46 +0300

    Создал класс для хранения данных рекордов

---
+++ SaperXoxoev/SaperXoxoev/RecordsData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaperXoxoev
+{
+    public class RecordsData
+    {
+        public int EasyRecord { get; set; } = 0;     
+        public int MediumRecord { get; set; } = 0;   
+        public int HardRecord { get; set; } = 0;     
+        public int ExpertRecord { get; set; } = 0;  
+
+        // Количество сыгранных партий (для статистики)
+        public int TotalGames { get; set; } = 0;
+        public int Wins { get; set; } = 0;
+        public int Losses { get; set; } = 0;
+    }
+}
